175a5507fbc0c12d7a925a1b1830c08b8a26c495
favourbadakin <56599038+favourbadakin@users.noreply.github.com>
2022-09-24 15:04:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Try from './Try';
 import Footer from './Footer'
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
