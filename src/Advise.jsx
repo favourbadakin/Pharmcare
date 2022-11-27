f3f9e2001630e58012d0499a230df901b2ce2985
favourbadakin <56599038+favourbadakin@users.noreply.github.com>
2022-11-27 08:39:17 +0100

    Update Advise.jsx

--- src/Advise.jsx
+++ src/Advise.jsx
@@ -9,6 +9,7 @@ import wink from './images/wink.svg';
 
 export default function Advise(){
 return(
+
     <section className='advise--section'>
         <div className='rect-wrap'>
                 <img className='rectangle' src={rectangle} alt='rectangle' />
