a4a624fd830835f6f4ba54680a4135c190740a72
favourbadakin <56599038+favourbadakin@users.noreply.github.com>
2023-02-12 16:03:31 +0100

    Update Try.jsx

--- src/Try.jsx
+++ src/Try.jsx
@@ -4,7 +4,6 @@ import google from './images/google.png';
 import apple from './images/apple.svg';
 export default function Header (){
 return(
-
     <section className='try--section'>
         <div className='try-div'>
             <h5>Be our Success story</h5>
