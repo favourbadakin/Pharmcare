0261da1872c4d937aa3e60f96b6bd2762fcad96d
favourbadakin <56599038+favourbadakin@users.noreply.github.com>
2023-03-18 13:34:44 +0100

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,6 @@ import { Squash as Hamburger } from 'hamburger-react'
 export default function Header(){
 return(
     <header>
-
         <div className='first-half'>
         <nav>
             <img className='nav-logo' src={logo} alt='logo' />
